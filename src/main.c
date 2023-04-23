a7eee6e97c19cb78b184da9be2e3c16f00d7d9b9
João Vitor Espig <jotinha1300@gmail.com>
2023-04-23 18:10:39 -0300

    changed: now main.c is a REPL

--- src/main.c
+++ src/main.c
@@ -7,24 +7,47 @@
 #include "parser/parser.h"
 #include "parser/ast.h"
 #include "evaluator/evaluator.h"
+
 int main()
 {
     lexer_text_t buff = (lexer_text_t)malloc(sizeof(char) * 128);
-    fgets(buff, 128, stdin);
-    buff[strcspn(buff, "\n")] = 0; // removes \n
-    if (strcmp(buff, "") == 0)
-    {
-        printf("Invalid input\n");
-        exit(1);
-    }
-    printf("%s\n", buff);
-    token_list_t *tokens = generate_tokens(buff);
-    token_list_print(tokens);
 
-    parser_t *parser = new_parser(tokens);
-    ast_node_t *ast = parser_generate_ast(parser);
-    int result = evaluate_ast(ast);
-    printf("Result of the operation: %d\n", result);
+    printf("MATH INTERPRETER\n"
+           "by: JotaEspig\n"
+           "Version: v0.1\n"
+           "----\n"
+           "type \"exit\" or \"q\" to exit the program\n"
+           "======================================\n");
+    while (1)
+    {
+        printf("> ");
+        fgets(buff, 128, stdin);
+        buff[strcspn(buff, "\n")] = 0; // removes \n
+        if (strcmp(buff, "q") == 0 || strcmp(buff, "exit") == 0)
+            exit(0);
+
+        if (strcmp(buff, "") == 0)
+        {
+            printf("Invalid input\n");
+            continue;
+        }
+
+        token_list_t *tokens = generate_tokens(buff);
+        //printf("TOKENS:\n");
+        //token_list_print(tokens);
+        //printf("\n");
+
+        parser_t *parser = new_parser(tokens);
+        ast_node_t *ast = parser_generate_ast(parser);
+        if (ast == NULL)
+        {
+            printf("Invalid syntax\n");
+            continue;
+        }
+
+        int result = evaluate_ast(ast);
+        printf("= %d\n", result);
+    }
 
     return 0;
 }
