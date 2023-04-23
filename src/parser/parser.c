b8c7a0a69695829bf132b45a6b97df7ad5d63362
João Vitor Espig <jotinha1300@gmail.com>
2023-04-23 13:15:17 -0300

    added: simple parser (don't handle +|- Factor)

--- src/parser/parser.c
+++ src/parser/parser.c
@@ -25,3 +25,96 @@ ast_node_t *parser_generate_ast(parser_t *parser)
     ast_node_t *result = parser_parse_expr(parser);
     return result;
 }
+
+ast_node_t *parser_parse_expr(parser_t *parser)
+{
+    ast_node_t *result = parser_parse_term(parser);
+    if (result == NULL)
+        return NULL;
+
+    while (parser->current_token.type != INVALID &&
+           (parser->current_token.type == PLUS ||
+            parser->current_token.type == MINUS))
+    {
+        if (parser->current_token.type == PLUS)
+        {
+            next(parser);
+            ast_node_t *right = parser_parse_term(parser);
+            if (right == NULL)
+                return NULL;
+
+            result = new_plus_node(result, right);
+        }
+        else if (parser->current_token.type == MINUS)
+        {
+            next(parser);
+            ast_node_t *right = parser_parse_term(parser);
+            if (right == NULL)
+                return NULL;
+
+            result = new_minus_node(result, right);
+        }
+    }
+
+    return result;
+}
+
+ast_node_t *parser_parse_term(parser_t *parser)
+{
+    ast_node_t *result = parser_parse_factor(parser);
+    if (result == NULL)
+        return NULL;
+
+    while (parser->current_token.type != INVALID &&
+           (parser->current_token.type == MULTIPLY ||
+            parser->current_token.type == DIVIDE))
+    {
+        if (parser->current_token.type == MULTIPLY)
+        {
+            next(parser);
+            ast_node_t *right = parser_parse_factor(parser);
+            if (right == NULL)
+                return NULL;
+
+            result = new_multiply_node(result, right);
+        }
+        else if (parser->current_token.type == DIVIDE)
+        {
+            next(parser);
+            ast_node_t *right = parser_parse_factor(parser);
+            if (right == NULL)
+                return NULL;
+
+            result = new_divide_node(result, right);
+        }
+    }
+
+    return result;
+}
+
+ast_node_t *parser_parse_factor(parser_t *parser)
+{
+    ast_node_t *result = NULL;
+    if (parser->current_token.type == INVALID)
+        return NULL;
+
+    if (parser->current_token.type == LPAREN)
+    {
+        next(parser);
+        result = parser_parse_expr(parser);
+        if (parser->current_token.type == RPAREN)
+        {
+            next(parser);
+            return result;
+        }
+
+        return NULL;
+    }
+
+    if (parser->current_token.type != NUMBER)
+        return NULL;
+
+    result = new_number_node(parser->current_token);
+    next(parser);
+    return result;
+}
