0ba382a036357fbc6b23a1cc26a6464178ca0e66
João Vitor Espig <jotinha1300@gmail.com>
2023-04-23 17:15:43 -0300

    added: evaluator for the ast

---
+++ src/evaluator/evaluator.c
@@ -0,0 +1,31 @@
+#include "evaluator.h"
+
+#include "../parser/ast.h"
+#include "../lexer/tokens.h"
+
+int evaluate_ast(ast_node_t *node)
+{
+    if (node == NULL)
+        return 0;
+
+    switch (node->token.type)
+    {
+    case NUMBER:
+        return node->token.value;
+
+    case PLUS:
+        return evaluate_ast(node->left) + evaluate_ast(node->right);
+
+    case MINUS:
+        return evaluate_ast(node->left) - evaluate_ast(node->right);
+
+    case MULTIPLY:
+        return evaluate_ast(node->left) * evaluate_ast(node->right);
+
+    case DIVIDE:
+        return evaluate_ast(node->left) / evaluate_ast(node->right);
+
+    default:
+        return 0;
+    }
+}
---
+++ src/evaluator/evaluator.h
@@ -0,0 +1,8 @@
+#ifndef EVALUATOR_H
+#define EVALUATOR_H
+
+#include "../parser/ast.h"
+
+int evaluate_ast(ast_node_t *node);
+
+#endif // EVALUATOR_H
