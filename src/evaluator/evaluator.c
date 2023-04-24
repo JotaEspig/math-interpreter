26edcafd9c242e4f3b622193f2efb7e2d329d88c
João Vitor Espig <jotinha1300@gmail.com>
2023-04-24 01:46:57 -0300

    added: check if divisor is 0 (it's not the best of all worlds)

--- src/evaluator/evaluator.c
+++ src/evaluator/evaluator.c
@@ -8,6 +8,7 @@ int evaluate_ast(ast_node_t *node)
     if (node == NULL)
         return 0;
 
+    int divisor;
     switch (node->token.type)
     {
     case NUMBER:
@@ -23,7 +24,8 @@ int evaluate_ast(ast_node_t *node)
         return evaluate_ast(node->left) * (unsigned)evaluate_ast(node->right);
 
     case DIVIDE:
-        return evaluate_ast(node->left) / evaluate_ast(node->right);
+        divisor = evaluate_ast(node->right);
+        return divisor ? evaluate_ast(node->left) / divisor : 0;
 
     default:
         return 0;
